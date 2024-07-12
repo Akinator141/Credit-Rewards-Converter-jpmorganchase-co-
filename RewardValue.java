5d6e10ec6e39c4211b98328c5135888a06358791
Abhishek .P.Kshirsagar <148625954+Akinator141@users.noreply.github.com>
2024-07-12 18:23:53 +0530

    credit Reward Converter

---
+++ RewardValue.java
@@ -0,0 +1,12 @@
+package mainfile;
+
+public class RewardValue {
+    public RewardValue(double cashValue){
+        getMilesValue();
+    }
+   public void getMilesValue() {
+
+        int cashValue =0;
+        double milesValue =  cashValue * 0.01;
+    }
+}
---
+++ RewardsConverter.java
@@ -0,0 +1,26 @@
+package mainfile;
+
+import java.lang.String;
+import java.util.Scanner;
+
+public class RewardsConverter {
+    public static void main(String[] args) {
+        Scanner Scanner=new Scanner(System.in);
+        System.out.println("Welcome to the Credit Card Rewards Converter!");
+        System.out.println("Please enter a cash value to convert to airline miles: ");
+        var  input_value =Scanner.nextLine();
+        double cashValue;
+        try {
+
+            cashValue = Double.parseDouble(input_value);
+        } catch (NumberFormatException exception) {
+            System.out.println("Could not parse input value as a double, exiting");
+            return;
+        }
+        System.out.println("converting $" + input_value + " to miles");
+        RewardValue RewardsValue;
+        RewardsValue = new RewardValue(cashValue);
+        System.out.println("$" + input_value + " is worth ");
+        RewardsValue.getMilesValue();
+    }
+}
